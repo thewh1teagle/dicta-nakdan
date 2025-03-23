25225b7f91b04ee0f39573d1ee35c567e393b4c1
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-03-23 19:37:57 +0200

    knesset

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 from transformers import AutoModel, AutoTokenizer
+import torch
 import time
+from tqdm import tqdm
 
-
+# Load model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained('dicta-il/dictabert-large-char-menaked')
 model = AutoModel.from_pretrained('dicta-il/dictabert-large-char-menaked', trust_remote_code=True)
 
+# Move model to GPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
 model.eval()
 
-start = time.time()
-sentence = 'בשנת 1948 השלים אפרים קישון את לימודיו בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים'
-print(model.predict([sentence], tokenizer))
-print(f'Time: {time.time() - start}')
+# Prepare your sentence
+sentence = 'בשנת 1948 השלים אפרים קישון את לימודיו בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים' * 10
+
+# Tokenize once (if sentence doesn’t change)
+inputs = tokenizer(sentence, return_tensors="pt", truncation=True, padding=True).to(device)
+# Warmup (optional but helps for benchmarking)
+model.predict(sentence, tokenizer)
+
+
+total = 0
+with open('knesset.txt') as fp:
+    for line in fp:
+        total += 1
+
+with open('knesset.txt') as fp, open('knesset_niqqud.txt', 'w') as out:
+    for line in tqdm.tqdm(fp, total=total):
+        line = line.strip()
+        if not line:
+            continue
+        try:
+            out.write(model.predict([line], tokenizer) + '\n')
+            out.flush()
+        except Exception as e:
+            print(f'Error: {e}')
+            out.write('\n')
+            
+        
+    
+    
+    
