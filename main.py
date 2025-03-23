5df19dcdf1a9d866cd702de9afe5165b4157c039
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-03-23 19:09:07 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from transformers import AutoModel, AutoTokenizer
+import time
+
+
+tokenizer = AutoTokenizer.from_pretrained('dicta-il/dictabert-large-char-menaked')
+model = AutoModel.from_pretrained('dicta-il/dictabert-large-char-menaked', trust_remote_code=True)
+
+model.eval()
+
+start = time.time()
+sentence = 'בשנת 1948 השלים אפרים קישון את לימודיו בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים'
+print(model.predict([sentence], tokenizer))
+print(f'Time: {time.time() - start}')
