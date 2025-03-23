6d4fd096decbb472f671c38cc8f01d02b4818d03
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-03-23 20:01:17 +0200

    Fix assertion error message in batch processing to show niqqud removal results

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ with open('knesset.txt') as fp, open('knesset_niqqud.txt', 'w') as out:
                 results = model.predict(batch, tokenizer)
                 # assert all lines without niqqud eaual to the original and show differences
                 for i, (original, result) in enumerate(zip(batch, results)):
-                    assert remove_niqqud(original) == remove_niqqud(result), f'{original} != {result}'
+                    assert remove_niqqud(original) == remove_niqqud(result), f'{remove_niqqud(original)} != {remove_niqqud(result)}'
                 out.writelines([r + '\n' for r in results])
                 out.flush()
             except Exception as e:
